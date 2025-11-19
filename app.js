25cb0b3b62921e1aa815f8f3deb3da0c9f1389ca
Aryan Saini <09aryan.lucky@gmail.com>
2025-11-19 15:33:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+console.log(a);
+var a;
+// let a;
+*/
+
+/*
+// Relationship b/w number and string
+let a = 10;
+let b = 20;
+
+// console.log("sum of 10 and 20 is " + a + b); 
+console.log("sum of 10 and 20 is " + (a + b)); 
+// console.log(a + b + "sum of 10 and 20"); 
+*/
+
+// swapping variabes
+// We can can use different methods for ex...
+// 1) using temp. variable
+// 2) using arithematic operators
+// 3) destructuring
